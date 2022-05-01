1b2c76cb1465bd5f88ec96f53fa734974ba12bd6
Allan <allangeek@gmail.com>
2022-05-01 18:09:38 -0300

    Melhorando a performance do ProductItem usando memo

--- components/ProductItem.tsx
+++ components/ProductItem.tsx
@@ -1,3 +1,5 @@
+import { memo } from 'react';
+
 interface ProductItemProps {
   product: {
     id: number;
@@ -6,10 +8,25 @@ interface ProductItemProps {
   };
 }
 
-export function ProductItem({ product }: ProductItemProps) {
+// shallow compare
+
+function ProductItemComponent({ product }: ProductItemProps) {
   return (
     <div>
       {product.title} - <strong>{product.price}</strong>
     </div>
   );
 }
+
+export const ProductItem = memo(
+  ProductItemComponent,
+  (prevProps, nextProps) => {
+    return Object.is(prevProps.product, nextProps.product);
+  }
+);
+
+// memo - when use it
+/**
+ * 1. Pure Functional components (components that abstracts a visual portion of the app. Functions that as the params are the same, always returns same result)
+ * 2. Components that renders too often
+ */
