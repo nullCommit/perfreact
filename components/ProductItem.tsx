2a1946a3d7a316dad57a9693598926b730caa115
Allan <allangeek@gmail.com>
2022-05-01 18:39:52 -0300

    Melhorando a performance do SearchResults com o useMemo

--- components/ProductItem.tsx
+++ components/ProductItem.tsx
@@ -27,6 +27,8 @@ export const ProductItem = memo(
 
 // memo - when use it
 /**
- * 1. Pure Functional components (components that abstracts a visual portion of the app. Functions that as the params are the same, always returns same result)
+ * 1. Pure Functional components (components that abstracts a visual portion of the app. Functions that as the props are the same, always returns same result)
  * 2. Components that renders too often
+ * 3. Components re-renders with same props
+ * 4. Component medium to large size
  */
